9e46947a86b45ed8919096738091af1c150777b9
unknown <19623180@std.stei.itb.ac.id>
2024-12-04 12:29:40 +0700

    feat: Edit Event di Mengelola Acara

--- src/UC01MengelolaAcara.py
+++ src/UC01MengelolaAcara.py
@@ -11,7 +11,7 @@ event_display = EventDisplay(controller.get_event_list())
 controller.add_event("1", "Location A", "2023-10-01", "Belum dimulai")
 controller.add_event("2", "Location B", "2023-10-02", "Sedang berlangsung")
 controller.add_event("3", "Location C", "2023-10-03", "Sudah selesai")
-
+event_display.event_list = controller.get_event_list()
 # Update the event list in EventDisplay
 # event_display.event_list = controller.get_event_list()
 
@@ -23,9 +23,8 @@ controller.add_event("3", "Location C", "2023-10-03", "Sudah selesai")
 
 while True:
     user_input = input(">>> ")
-    if user_input.upper == "SORT":
-        event_display.display_sort_options()
-        user_input = input("Sort by >>> ")
+    if user_input.upper() == "LIST":
+        event_display.display_event_list()
     elif user_input.upper() == "ADD":
         form_event = FormEvent()
         form_data = form_event.display_form()
@@ -34,5 +33,17 @@ while True:
         controller.add_event(event_data["EventID"], event_data["EventLocation"], event_data["EventDate"], event_data["EventStatus"])
         event_display.event_list = controller.get_event_list()
         event_display.display_event_list()
+    elif user_input.upper() == "EDIT":
+        event_id = input("Masukkan ID acara yang ingin diedit: ")
+        if controller.get_event_details(event_id) is None:
+            print(f"Error: Event dengan ID '{event_id}' tidak ditemukan.")
+        else:
+            form_event = FormEvent()
+            form_data = form_event.display_form()
+            form_event.submit_form(form_data)
+            if form_event.event_details:
+                controller.edit_event(event_id, **form_event.event_details)
+                event_display.event_list = controller.get_event_list()
+                event_display.display_event_list()
     elif user_input.upper() == "EXIT":
         break
