9fdff9f3babe83f4c7827235df38c8f1c2ff9907
angelinaefrina <efrinaprahastaputri@gmail.com>
2024-12-14 16:18:28 +0700

    fix: path in manage guest

--- src/pages/manage_guest.py
+++ src/pages/manage_guest.py
@@ -1,14 +1,11 @@
 import sys
 import os
-
-# Add the src directory to the sys.path
-#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 import flet as ft
-from guestcontroller import GuestController
-from guestlistform import GuestListForm
-from utils.buttons import *
-from utils.pagesetup import PageSetup
+from src.database.guestcontroller import GuestController
+from src.database.guestlistform import GuestListForm
+from src.utils.buttons import *
+from src.utils.pagesetup import PageSetup
 
 ITEMS_PER_PAGE = 6
 class GuestManagerApp:
